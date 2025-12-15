0061d0613a615879b1bb470e6569e6c822f45e83
Zakhar Yakusevich <fnemekov@mail.ru>
2025-12-15 14:33:45 +0300

    main and russian

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <windows.h>
 
 using namespace std;
 
@@ -38,3 +40,46 @@ public:
         salary = newSalary;
     }
 };
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const int ARRAY_SIZE = 5;
+    vector<Employee> employees;
+    
+    cout << "Введите данные для трёх сотрудников:" << endl;
+    
+    for (int i = 0; i < 3; i++) 
+    {
+        string name;
+        int id;
+        double salary;
+        
+        cout << "\nСотрудник " << (i + 1) << ":" << endl;
+        
+        cout << "Имя: ";
+        getline(cin, name);
+        
+        cout << "ID: ";
+        cin >> id;
+        
+        cout << "Зарплата: ";
+        cin >> salary;
+        
+        cin.ignore();
+        
+        employees.push_back(Employee(name, id, salary));
+    }
+    
+    cout << "\nСписок всех сотрудников:" << endl;
+    
+    for (int i = 0; i < employees.size(); i++) 
+    {
+        cout << "ID: " << employees[i].getId() 
+             << ", Name: " << employees[i].getName() 
+             << ", Salary: " << employees[i].getSalary() << endl;
+    }
+    
+    return 0;
+}
